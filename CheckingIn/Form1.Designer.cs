526229e6016bac8adb6be61448cd8f816c8d4bc6
ningxiaoxiao <732869825@qq.com>
2016-09-14 16:53:19 +0800

    完成基本功能

--- CheckingIn/Form1.Designer.cs
+++ CheckingIn/Form1.Designer.cs
@@ -28,12 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "readfile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "考勤机器文件|*.xls";
+            this.openFileDialog1.Title = "选择考勤文件";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.BackColor = System.Drawing.Color.Salmon;
+            this.monthCalendar1.ForeColor = System.Drawing.Color.Red;
+            this.monthCalendar1.Location = new System.Drawing.Point(254, 12);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.ShowToday = false;
+            this.monthCalendar1.ShowTodayCircle = false;
+            this.monthCalendar1.TabIndex = 4;
+            this.monthCalendar1.TitleForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.monthCalendar1.TodayDate = new System.DateTime(2016, 7, 5, 0, 0, 0, 0);
+            this.monthCalendar1.DateSelected += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateSelected);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(93, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(158, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 201);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(462, 58);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.VirtualMode = true;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "name";
+            this.columnHeader1.Width = 81;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "date";
+            this.columnHeader2.Width = 98;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "in";
+            this.columnHeader3.Width = 129;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "out";
+            this.columnHeader4.Width = 125;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5,
+            this.columnHeader7,
+            this.columnHeader6});
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(12, 289);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(462, 254);
+            this.listView2.TabIndex = 9;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.VirtualMode = true;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "name";
+            this.columnHeader5.Width = 80;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "time";
+            this.columnHeader6.Width = 251;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "处理后的数据";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "原始数据";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "date";
+            this.columnHeader7.Width = 99;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 555);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
     }
 }
 
--- CheckingIn/Form1.cs
+++ CheckingIn/Form1.cs
@@ -1,20 +1,225 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace CheckingIn
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// 处理后的表格
+        /// </summary>
+        private DataTable _resultdt;
+        /// <summary>
+        /// 原始表格
+        /// </summary>
+        private DataTable _xlsdt;
+        /// <summary>
+        /// 列表使用的
+        /// </summary>
+        private DataTable _listResultdt;
+        /// <summary>
+        /// 列表使用的
+        /// </summary>
+        private DataTable _listdt;
+
         public Form1()
         {
+            CheckForIllegalCrossThreadCalls = false;
             InitializeComponent();
+            openFileDialog1.FileOk += OpenFileDialog1_FileOk;
+            listView1.RetrieveVirtualItem += ListView1_RetrieveVirtualItem;
+            listView2.RetrieveVirtualItem += ListView2_RetrieveVirtualItem;
         }
+
+        private void ListView2_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            e.Item = new ListViewItem(new[] {
+                _listdt.Rows[e.ItemIndex]["name"].ToString(),
+                _listdt.Rows[e.ItemIndex]["date"].ToString(),
+                _listdt.Rows[e.ItemIndex]["time"].ToString(),
+            });
+        }
+
+        private void ListView1_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            e.Item = new ListViewItem(new[] {
+                _listResultdt.Rows[e.ItemIndex]["name"].ToString(),
+                _listResultdt.Rows[e.ItemIndex]["date"].ToString(),
+                _listResultdt.Rows[e.ItemIndex]["intime"].ToString(),
+                _listResultdt.Rows[e.ItemIndex]["outtime"].ToString(),
+            });
+        }
+
+        private void OpenFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                var dt = ExcelToDs(openFileDialog1.FileName);
+
+
+                //二次处理的表
+
+                _xlsdt = new DataTable();
+
+                _xlsdt.Columns.Add("name", typeof(string));
+                _xlsdt.Columns.Add("date", typeof(DateTime));
+                _xlsdt.Columns.Add("time", typeof(TimeSpan));
+
+                //进行遍历处理 生成新的名
+                foreach (DataRow i in dt.Tables[0].Rows)
+                {
+
+                    //读出时间
+                    var time = DateTime.Parse(i["日期时间"].ToString());
+                    //增加新记录
+                    var newr = _xlsdt.NewRow();
+                    newr["name"] = i["姓名"];
+                    newr["date"] = time.Date;
+                    newr["time"] = time.TimeOfDay;
+                    _xlsdt.Rows.Add(newr);
+                }
+
+
+
+                var t = new Thread(Changedata);
+                t.Start();
+            }
+            catch (Exception ex)
+            {
+
+                Console.Out.WriteLine(ex.ToString());
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        public DataSet ExcelToDs(string Path)
+        {
+            var strConn = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + Path + ";" + "Extended Properties=Excel 8.0;";
+            var conn = new OleDbConnection(strConn);
+            conn.Open();
+            var strExcel = "";
+            OleDbDataAdapter myCommand = null;
+            DataSet ds = null;
+            strExcel = "select * from [123$]";
+            myCommand = new OleDbDataAdapter(strExcel, strConn);
+            ds = new DataSet();
+            myCommand.Fill(ds, "table1");
+            return ds;
+        }
+
+
+        private void Changedata()
+        {
+            //结果表
+            _resultdt = new DataTable();
+
+            _resultdt.Columns.Add("name", typeof(string));
+            _resultdt.Columns.Add("date", typeof(DateTime));
+            _resultdt.Columns.Add("intime", typeof(TimeSpan));
+            _resultdt.Columns.Add("outtime", typeof(TimeSpan));
+
+
+
+
+            //读出所有姓名
+            var dv = new DataView(_xlsdt);
+            var namedt = dv.ToTable(true, "name");
+            //生成所有当月工作日
+
+            progressBar1.Maximum = namedt.Rows.Count;
+            progressBar1.Value = 0;
+            //对每人个进行遍历
+            foreach (DataRow r in namedt.Rows)
+            {
+                //当前人名字
+                var n = r["name"].ToString();
+                comboBox1.Items.Add(n);
+                //读出这个人所有记录
+                var rs = _xlsdt.Select("name = '" + n + "'");
+
+                //得到所有有人出勤的日期
+                var datedt = dv.ToTable(true, "date");
+
+
+                foreach (DataRow dater in datedt.Rows)
+                {
+                    //得到这个人今天所有的时间
+                    var date = dater["date"];
+
+                    var timeview = new DataView(_xlsdt) { RowFilter = $"name = '{n}' AND date = '{date}'" };
+                    var t = timeview.ToTable();
+
+                    var times = new ArrayList();
+
+                    foreach (DataRow timer in t.Rows)
+                    {
+                        times.Add(timer["time"]);
+                    }
+
+                    if (times.Count == 0) continue;//当天没有记录 返回
+
+                    times.Sort();
+                    //进行记录
+                    var rr = _resultdt.NewRow();
+                    rr["name"] = n;
+                    rr["date"] = date;
+                    rr["intime"] = times[0];
+                    rr["outtime"] = times[times.Count - 1];
+                    _resultdt.Rows.Add(rr);
+
+
+                    // Console.Out.WriteLine("{0}:{1}:{2}:{3}", n, date, times[0], times[times.Count - 1]);
+                }
+                progressBar1.Value += 1;
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //得到这个人所有的日期
+            var dv = new DataView(_resultdt) { RowFilter = $"name = '{comboBox1.Text}'" };
+
+            var days = dv.ToTable();
+
+            monthCalendar1.RemoveAllBoldedDates();
+            foreach (DataRow dr in days.Rows)
+            {
+                monthCalendar1.AddBoldedDate((DateTime)dr["date"]);
+            }
+            var v = monthCalendar1.BoldedDates[0];
+
+            monthCalendar1.SetDate(v.AddMonths(1));
+            monthCalendar1.SetDate(v);
+
+
+        }
+
+        private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            //得到这个用户 当天的记录
+            var dv = new DataView(_resultdt) { RowFilter = $"name = '{comboBox1.Text}' AND date = '{e.Start}'" };
+            _listResultdt = dv.ToTable();
+            listView1.VirtualListSize = _listResultdt.Rows.Count;
+            listView1.Invalidate();
+
+            //原来的记录
+            dv = new DataView(_xlsdt) { RowFilter = $"name = '{comboBox1.Text}' AND date = '{e.Start}'" };
+            _listdt = dv.ToTable();
+            listView2.VirtualListSize = _listdt.Rows.Count;
+            listView2.Invalidate();
+        }
+
+
     }
 }
